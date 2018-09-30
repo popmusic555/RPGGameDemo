3bf3a5dcb4c2dca58f215824bc85c790cfa24fe3
popmusic555 <zy3gprogramer@163.com>
2018-09-30 17:02:44 +0800

    微信小游戏平台远程获取资源关联列表工具 以防止更新资源时 settings.js 资源列表配置脚本也需要更新 导致必须审核
    后期加入关闭 打开功能
    
    只在微信小游戏平台打包才调用此功能

---
+++ packages/wxplatfor-builder/main.js
@@ -0,0 +1,109 @@
+﻿'use strict';
+
+var path = require('path');
+var fs = require('fs');
+
+function getRequireSetting(textArr)
+{
+	var len = textArr.length;
+	for(var index = 0; index < len; index++)
+	{
+		var scriptLine = textArr[index];
+		
+		var result = scriptLine.indexOf("src/settings");
+		if (result != -1)
+		{
+			// 检索到当前行
+			return index;
+		}
+	}
+	return -1;
+}
+
+function getSettingJsName(str)
+{
+	var matchStr = str.match(/['"][^'"]*['"]/g);
+	if (matchStr.length > 0)
+	{
+		matchStr = matchStr[0];
+		matchStr = matchStr.slice(1 , -1);
+	}
+	else
+	{
+		matchStr = null;
+	}
+	
+	return matchStr;
+}
+
+
+function onBeforeBuildFinish (options, callback) {
+    var gameJsPath = path.join(options.dest, 'game.js');  // 获取发布目录下的 main.js 所在路径
+    var script = fs.readFileSync(gameJsPath, 'utf8');     // 读取构建好的 main.js
+	
+	var textArr = script.split("\r\n");
+	
+	// 获取settings.js行
+	var index = getRequireSetting(textArr);
+
+	if(index != -1)
+	{
+		// 输出settings.js的json文件
+		// 获取settings.js文件名称
+		var name = getSettingJsName(textArr[index]);
+		if(name && name != "")
+		{
+			var settingJsonPath = path.join(options.dest, name + ".js");
+			var settingJsStr = fs.readFileSync(settingJsonPath, 'utf8');
+			var newSettingJsStr = settingJsStr.replace("window._CCSettings" , "module.exports");
+			fs.writeFileSync(settingJsonPath , newSettingJsStr);
+			
+			var settingJsObj = require(settingJsonPath);
+			var jsonStr = JSON.stringify(settingJsObj);			
+			fs.writeFileSync(path.join(options.dest, "res/settings.json") , jsonStr);
+			fs.writeFileSync(settingJsonPath , settingJsStr);
+		}
+		
+		// 改变settings.js 加载方式
+		var strArr1 = textArr.slice(0 , index);
+		var strArr2 = textArr.slice(index + 1);
+		
+		var len = strArr2.length;
+		for(var index = 0; index < len; index++)
+		{
+			strArr2[index] = "        " + strArr2[index];
+		}
+		
+		var newStrArr1 = [
+			"var xhr = new XMLHttpRequest();",
+			"xhr.onreadystatechange = function () {",
+			"    if (xhr.readyState == 4 && (xhr.status >= 200 && xhr.status < 400)) {",
+			"        var response = xhr.responseText;",
+			"        window._CCSettings = JSON.parse(response);"
+		];
+		
+		var newStrArr2 = [
+			"    }",
+			"}",
+			"xhr.open(\"GET\", '" + options.wechatgame.REMOTE_SERVER_ROOT + "/res/settings.json" + "', true);",
+			"xhr.send();"
+		];
+		
+		textArr = strArr1.concat(newStrArr1 , strArr2 , newStrArr2);
+	}
+	
+	script = textArr.join("\r\n");
+	fs.writeFileSync(gameJsPath, script);
+	
+    callback();
+}
+
+module.exports = {
+    load () {
+        Editor.Builder.on('build-finished', onBeforeBuildFinish);
+    },
+
+    unload () {
+        Editor.Builder.removeListener('build-finished', onBeforeBuildFinish);
+    }
+};
